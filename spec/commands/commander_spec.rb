9ff090f2513056feaac0382ae7cb6502efe526c2
Dennis Ideler <ideler.dennis@gmail.com>
2016-06-02 21:10:48 +0100

    Add more unit tests for Commander.execute

--- spec/commands/commander_spec.rb
+++ spec/commands/commander_spec.rb
@@ -6,17 +6,28 @@ RSpec.describe Commander do
     subject { Commander.execute(instructions, rover) }
     let(:rover) { double }
 
-    context 'when given known instructions'
+    context 'when given known instructions' do
+      let(:instructions) { 'LRM' }
+      it 'executes the relevant commands' do
+        expect(LeftCommand).to receive(:execute).with(rover).ordered
+        expect(RightCommand).to receive(:execute).with(rover).ordered
+        expect(MoveCommand).to receive(:execute).with(rover).ordered
+        subject
+      end
+    end
 
     context 'when given unknown instructions' do
       let(:instructions) { '💥' }
-      it 'raises' do
+      it 'raises InvalidInstruction' do
         expect { subject }.to raise_error(Commander::InvalidInstruction)
       end
     end
 
-    context 'when given no instructions'
-
-    context 'when instructions are missing'
+    context 'when instructions contain whitespace' do
+      let(:instructions) { ' ' }
+      it 'raises InvalidInstruction' do
+        expect { subject }.to raise_error(Commander::InvalidInstruction)
+      end
+    end
   end
 end
